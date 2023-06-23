709bc3445e9fb8983af03b0dbd3e7bfd0915f518
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-06-23 16:44:27 -0400

    more code

---
+++ firmware/adafruit-testing/src/defs.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define SENSOR_ADDR		0x4Au
+
+#define BAUD_RATE_HZ  400000 // Fast mode 400 kb/s
+
--- firmware/adafruit-testing/src/main.c
+++ firmware/adafruit-testing/src/main.c
@@ -1,8 +1,42 @@
-#include "pico/stdlib.h"
+#include <stdio.h>
 
-void main()
+#include "boards/pico.h"
+#include "hardware/gpio.h"
+#include "pico/stdio.h"
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+#include "defs.h"
+
+/**
+ * Initialise the I2C functionality
+ */
+void init(){
+
+#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
+#warning No I2C pins are set
+  puts("I2C pins were not defined")
+#else
+
+  i2c_init(i2c_default, BAUD_RATE_HZ);
+  
+  gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+  gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+
+  gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+  gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+
+#endif
+}
+
+int main()
 {
-  // TODO: main method stub
+
+  stdio_init_all();
+
+  init();
 
   for (;;) tight_loop_contents();
+
+  return 0;
 }
