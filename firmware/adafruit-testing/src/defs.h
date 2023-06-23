50ef34510ca7741ab4769b05ab0d9f60e8272491
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-06-23 16:58:23 -0400

    code

--- firmware/adafruit-testing/src/defs.h
+++ firmware/adafruit-testing/src/defs.h
@@ -4,3 +4,4 @@
 
 #define BAUD_RATE_HZ  400000 // Fast mode 400 kb/s
 
+#define REPORT_SIZE		256
--- firmware/adafruit-testing/src/main.c
+++ firmware/adafruit-testing/src/main.c
@@ -1,4 +1,6 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "boards/pico.h"
 #include "hardware/gpio.h"
@@ -7,6 +9,8 @@
 #include "hardware/i2c.h"
 
 #include "defs.h"
+
+uint8_t payload[REPORT_SIZE];
 
 /**
  * Initialise the I2C functionality
@@ -26,7 +30,31 @@ void init(){
   gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
   gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
 
+  printf("I2C interface successfully configured\n");
+
 #endif
+}
+
+/**
+ * Format and print the report stored in the payload
+ */
+void output_report(){
+  // TODO: Implement
+  printf("Report received successfully\n");
+}
+
+/**
+ * Polls the sensor over I2C and prints any output
+ */
+void poll_sensor(){
+  int res;
+
+  res = i2c_read_blocking(i2c_default, SENSOR_ADDR, &(payload[0]), REPORT_SIZE, false);
+
+  if(res == PICO_ERROR_GENERIC) printf("Failed to read from sensor\n");
+  else output_report();
+
+  memset(&payload, 0, REPORT_SIZE);
 }
 
 int main()
@@ -36,7 +64,7 @@ int main()
 
   init();
 
-  for (;;) tight_loop_contents();
+  for (;;) poll_sensor();
 
   return 0;
 }
