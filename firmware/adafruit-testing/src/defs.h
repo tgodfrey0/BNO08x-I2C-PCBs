9a6427840c1ce1e9126bc5643ece6df67ac6ced1
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-06-27 16:30:18 -0400

    reduced delay

--- firmware/adafruit-testing/src/defs.h
+++ firmware/adafruit-testing/src/defs.h
@@ -9,17 +9,17 @@
 #include <stdint.h>
 #define min(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a < _b ? _a : _b; })
 
-#define BNO085_ADDR					0b1001010U
+#define BNO085_ADDR	    0b1001010U
 
-#define I2C_INST						i2c0
-#define SDA_PIN							16
-#define SCL_PIN							17
-#define BAUD_RATE_HZ				400000 // Fast mode 400 kb/s
-#define TIMEOUT_MS					10000
+#define I2C_INST	    i2c0
+#define SDA_PIN		    16
+#define SCL_PIN		    17
+#define BAUD_RATE_HZ	    400000 // Fast mode 400 kb/s
+#define TIMEOUT_MS	    10000
 
-#define MAX_PAYLOAD_SIZE		512
-#define MAX_ATTEMPTS				5 // Max tries to open an I2C connection to the sensor
-#define SAMPLE_DELAY_MS			500 // Delay between reading the sensors
+#define MAX_PAYLOAD_SIZE    512
+#define MAX_ATTEMPTS	    5 // Max tries to open an I2C connection to the sensor
+#define SAMPLE_DELAY_MS	    100 // Delay between reading the sensors
 
 struct single_sensor_reports {
   uint8_t reports[MAX_PAYLOAD_SIZE];
